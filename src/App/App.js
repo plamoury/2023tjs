ec1e319ce69a804bc3c3aaef6a1b43356a7eb128
plamoury <pascal.lamoury@orange.com>
2023-01-23 15:30:21 +0100

    add img dans Button

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,17 @@ import Button from './components/ui/Button/Button';
 import './App.css';
 
 function App() {
+  /** Dans react le /App equivaut a /public
+   * qd react pousse les composant ils seront placés au meme niveau que public
+   * qd le path commence par "/" on vise le repertoire public
+   * sinon on descend dans les path ../../..
+   */
   return (
     <div className="App">
       Demat Breizh
       <hr/>
       <Button>ClickMe<br/>please</Button>
-      <Button>Cancel</Button>
+      <Button><img src="/img/deadicon.png" alt='sdfsdfsdf'/></Button>
     </div>
   );
 }
