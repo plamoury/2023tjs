77889aafe3a1eb7e6e320010ef4a73c4b35bdeef
plamoury <pascal.lamoury@orange.com>
2023-01-24 09:54:53 +0100

    usage de class garder l'etat ForceUpdate a eviter

--- src/App/App.js
+++ src/App/App.js
@@ -2,21 +2,47 @@ import React from 'react';
 import Button from './components/ui/Button/Button';
 import './App.css';
 
-function App() {
-  /** Dans react le /App equivaut a /public
-   * qd react pousse les composant ils seront placés au meme niveau que public
-   * qd le path commence par "/" on vise le repertoire public
-   * sinon on descend dans les path ../../..
-   */
-  return (
-    <div className="App">
-      Demat Breizh
-      <hr/>
-      <Button>ClickMe<br/>please</Button>
-      <Button><img src="/img/deadicon.png" alt='sdfsdfsdf'/></Button>
-      <Button children="toto"/>
-    </div>
-  );
+class App extends React.PureComponent {
+  counter = 0;
+  render() {
+    return (
+      <div className="App">
+        Voici le nb de click : {this.counter}
+        <hr />
+        <Button onButtonClick={() => {
+          this.counter--;
+          console.log(this.counter);
+          // mauvais usage parce que synchrone
+          // à eviter
+          this.forceUpdate();
+        }}>Click+1</Button>
+        <Button>Click-1</Button>
+      </div>
+    );
+  }
 }
+// function App() {
+//   /** Dans react le /App equivaut a /public
+//    * qd react pousse les composant ils seront placés au meme niveau que public
+//    * qd le path commence par "/" on vise le repertoire public
+//    * sinon on descend dans les path ../../..
+//    */
+//   let counter = 0;
+
+//   // besoin d'une valeur etatique 
+//   // avant on ne pouvait le faire qu'avec les class (désué)
+//   // mais maintenant on utilise les valeur etatique
+//   return (
+//     <div className="App">
+//       Voici le nb de click : {counter}
+//       <hr />
+//       <Button onButtonClick={() => {
+//         counter--;
+//         console.log(counter);
+//       }}>Click+1</Button>
+//       <Button>Click-1</Button>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -5,9 +5,14 @@ import style from "./Button.module.css"
  * simple button
  * @returns react component structure
  */
-const Button = (props)=> {
+const Button = (props) => {
     console.log(props);
-    return <button className={style.Button}>{props.children}</button>
+    return <button className={style.Button} onClick={(arg) => {
+        console.log(arg);
+        props.onButtonClick();
+    }}>
+        {props.children}
+    </button>
 }
 
 //function Button() {
