098320f116d8fa577bbffad306f207ca2fc7241b
plamoury <pascal.lamoury@orange.com>
2023-01-24 17:31:34 +0100

    1er et 2 fils + yarn npm i -g yarn

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
+import MemeForm from "./components/feature/MemeForm/MemeForm";
+import FlexHLayout from "./components/layout/FlexHLayout/FlexHLayout";
+import MemeSvgViewer from "./components/ui/MemeSvgViewer/MemeSvgViewer";
 
 interface IAppState {}
 interface IAppProps {}
 
 class App extends React.PureComponent<IAppProps, IAppState> {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <FlexHLayout>
+          <MemeSvgViewer />
+          <MemeForm />
+        </FlexHLayout>
+      </div>
+    );
   }
 }
 export default App;
--- src/App/components/layout/FlexHLayout/FlexHLayout.tsx
+++ src/App/components/layout/FlexHLayout/FlexHLayout.tsx
@@ -1,30 +1,32 @@
-import React,  { useEffect, useState } from 'react'
-import style from './FlexHLayout.module.css'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import style from "./FlexHLayout.module.css";
+import PropTypes from "prop-types";
 
-interface IFlexHLayoutProps{
-    myStyle?:{}
+interface IFlexHLayoutProps {
+  myStyle?: {};
+  children: Array<React.ReactElement>;
 }
-export interface IFlexHLayoutState { }
-export const initialState: IFlexHLayoutState = {}
+export interface IFlexHLayoutState {}
+export const initialState: IFlexHLayoutState = {};
 const FlexHLayout: React.FC<IFlexHLayoutProps> = (props) => {
-    const [state, setstate] = useState(initialState);
-    useEffect(() => {
-        //cmpdidmount
-    }, []);
-    return (
-    <div className={style.FlexHLayout}
+  const [state, setstate] = useState(initialState);
+  useEffect(() => {
+    //cmpdidmount
+  }, []);
+  return (
+    <div
+      className={style.FlexHLayout}
       style={props.myStyle}
-      data-testid="FlexHLayout">
-        flexHLayout
-    </div>)
+      data-testid="FlexHLayout"
+    >
+      {props.children}
+    </div>
+  );
 };
 
 FlexHLayout.propTypes = {
-    myStyle: PropTypes.object,
-}
-FlexHLayout.defaultProps = {
-
-}
-FlexHLayout.defaultProps = {}
+  myStyle: PropTypes.object,
+  children: PropTypes.any.isRequired,
+};
+FlexHLayout.defaultProps = {};
 export default FlexHLayout;
